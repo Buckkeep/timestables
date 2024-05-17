a8de5f0dfd56ec0ad6b6f481ac7564f4d016b63e
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-05-17 13:09:01 +0100

    Initial Commit

---
+++ timestables/timestablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  timestablesApp.swift
+//  timestables
+//
+//  Created by Buhecha, Neeta (Trainee Engineer) on 17/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct timestablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
