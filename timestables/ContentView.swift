626e907afbb47a01cfa41a01ce8aafc947549feb
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-05-17 14:46:25 +0100

    Base game logic

--- timestables/ContentView.swift
+++ timestables/ContentView.swift
@@ -7,13 +7,42 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    
+    @State private var value1: Int = 2
+    @State private var value2: Int = 2
+    
+    var displayValue1: Int { value1 + 2 }
+    var displayValue2: Int { value2 + 2 }
+    
+    var answer: Int { (value1 + 2)  * (value2 + 2) }
+    
     var body: some View {
-        VStack {
+        Form {
+  
+            Section("First number") {
+                Picker("Pick your first value", selection: $value1) {
+                    ForEach(2..<13) {
+                        Text("\($0)")
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            
+            Section("Second number") {
+                Picker("Pick your second value", selection: $value2) {
+                    ForEach(2..<13) {
+                        Text("\($0)")
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("\(displayValue1) x \(displayValue2) is \(answer)!")
         }
         .padding()
     }
