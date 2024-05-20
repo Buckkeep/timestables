f5683192494ea82bd5c6a124f757f5ad8c82efee
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-05-20 11:54:35 +0100

    Create grid struct to make custom grid. Changed spacing within struct to fit multiple images in.

--- timestables/ContentView.swift
+++ timestables/ContentView.swift
@@ -8,6 +8,32 @@
 import SwiftUI
 
 
+struct GridStack<Content: View>: View {
+    let rows: Int
+    let columns: Int
+    let content: (Int, Int) -> Content
+
+    var body: some View {
+        VStack(spacing: -100) {
+            ForEach(0 ..< rows, id: \.self) { row in
+                HStack(spacing: -100) {
+                    ForEach(0 ..< columns, id: \.self) { column in
+                        content(row, column)
+                    }
+                }
+            }
+        }
+    }
+
+    init(rows: Int, columns: Int, @ViewBuilder content: @escaping (Int, Int) -> Content) {
+        self.rows = rows
+        self.columns = columns
+        self.content = content
+    }
+}
+
+
+
 struct ContentView: View {
     
     @State private var value1: Int = 2
@@ -21,34 +47,35 @@ struct ContentView: View {
     var body: some View {
         VStack {
             
-            Text("How many flocks of ducks?")
-                Picker("Pick your first value", selection: $value1) {
-                    ForEach(2..<13) {
-                        Text("\($0)")
+            VStack(alignment: .center, content: {
+                Text("How many flocks of ducks?")
+                    Picker("Pick your first value", selection: $value1) {
+                        ForEach(2..<13) {
+                            Text("\($0)")
+                        }
                     }
-                }
-                .pickerStyle(.segmented)
-
+                    .pickerStyle(.segmented)
+                
+                Text("How many ducks per flock?")
+                    Picker("Pick your second value", selection: $value2) {
+                        ForEach(2..<13) {
+                            Text("\($0)")
+                        }
+                    }
+                    .pickerStyle(.segmented)
+            })
             
-            Text("How many ducks per flock?")
-                Picker("Pick your second value", selection: $value2) {
-                    ForEach(2..<13) {
-                        Text("\($0)")
+                
+            GridStack(rows: displayValue1, columns: displayValue2) { row, col in
+                    Image("duck")
+                        .scaleEffect(0.2)
+                        .padding(0)
                     }
-                }
-                .pickerStyle(.segmented)
             
-            HStack {
-                ForEach(1..<displayValue1, id: \.self) {_ in
-                    ForEach(1..<displayValue2, id: \.self) {_ in
-                        Image("duck")
-                            .scaleEffect(0.3)
-                    }
-                }
-                .padding(0)
-            }
             
             Text("\(displayValue1) flocks of \(displayValue2) ducks is a total of \(answer) ducks!")
+
+
         }
         .padding()
         .backgroundStyle(.opacity(0.3))
